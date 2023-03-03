6ede8663012f00a75084c729a195e3e1773eb8e9
ezravkatz <ezravkatz@gmail.com>
2023-03-03 14:14:40 -0500

    styling changes and reconfiguring logic

--- tictactoe.js
+++ tictactoe.js
@@ -1,26 +1,28 @@
-const ticTacToeGame = new TicTacToeGame();
-ticTacToeGame.start();
+var board = new Board();
+const positions = Array.from(document.querySelectorAll(".col"));
+const player = new humanPlayer(board);
+const cpu = new cpuPlayer(board);
+var turn = 0;
 
-function ticTacToeGame() {
-  const board = new Board();
-  const player = new Player(board);
-  const cpu = new CPU(board);
-  let turn = 0;
+startTicTacToeGame();
 
+function startTicTacToeGame() {
   this.start = function () {
+    //observe change of turn with observer
     const config = { childList: true };
     const observer = new MutationObserver(() => takeTurn());
-    board.positions.forEach((el) => observer.observe(el, config));
+    board?.positions.forEach((el) => observer.observe(el, config));
     takeTurn();
   };
+
   function takeTurn() {
     if (board.checkWinner()) {
       return;
     }
     if (turn % 2 === 0) {
-      player.takeTurn();
+      humanPlayer.takeTurn();
     } else {
-      cpu.takeTurn();
+      cpuPlayer.takeTurn();
     }
     turn++;
   }
@@ -44,9 +46,11 @@ function Board() {
     const positions = this.positions;
 
     winningCombos.forEach((winningCombo) => {
+      //cycle through winning combos, retrieve the inner text for each position
       const pos0InnerText = positions[winningCombo[0]].innerText;
       const pos1InnerText = positions[winningCombo[1]].innerText;
       const pos2InnerText = positions[winningCombo[2]].innerText;
+      //evaluate for a win
       const isWinningCombo =
         pos0InnerText !== "" &&
         pos0InnerText === pos1InnerText &&
@@ -64,20 +68,24 @@ function Board() {
   };
 }
 
-function Player(board) {
+function humanPlayer(board) {
   this.takeTurn = function () {
     board.positions.forEach((el) => el.addEventListener("click", turnTaken));
   };
 
   function turnTaken(event) {
+    console.log("turn taken");
     event.target.innerText = "X";
     board.positions.forEach((el) => el.removeEventListener("click", turnTaken));
   }
 }
 
-function CPU(board) {
+function cpuPlayer(board) {
+  console.log("cpu turn");
   this.takeTurn = function () {
+    //filter for positions where there is no inner text and the position is open
     const openPositions = board.positions.filter((p) => p.innerText === "");
+    //get a random integer and multiply that by the open positions to create a random computer move choice
     const move = Math.floor(Math.random() * openPositions.length);
     openPositions[move].innerText = "O";
   };
